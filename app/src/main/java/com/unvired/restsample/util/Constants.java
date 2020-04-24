b3debf27d434449be6243b908e460beb36805cb5
bhagyashree <bhagyashree.naik@unvired.io>
2020-04-24 13:32:29 +0530

    Updated metadata.xml and PA function name

--- app/src/main/java/com/unvired/restsample/util/Constants.java
+++ app/src/main/java/com/unvired/restsample/util/Constants.java
@@ -6,7 +6,7 @@ package com.unvired.sample.rest.util;
 
 public class Constants {
     // App Name
-    public static final String APPLICATION_NAME = "UnviredRESTSample";
+    public static final String APPLICATION_NAME = "UNVIRED_REST_SAMPLE";
     public static final String APPLICATION_TITLE = "Unvired REST Sample";
 
     // BE Names
@@ -14,7 +14,7 @@ public class Constants {
 
 
     // PA Function Names
-    public static final String PA_GET_WEATHER = "UNVIREDRESTSAMPLE_PA_GET_WEATHER"; // Get Weather
+    public static final String PA_GET_WEATHER = "UNVIRED_REST_SAMPLE_PA_GET_WEATHER"; // Get Weather
 
     public static final String RESPONSE_CODE_ERROR = "ERROR";
     public static final String RESPONSE_CODE_SUCCESSFUL = "SUCCESS";
